27cac8dd4226b8ff7776b8633b40c3561b4b0053
16 Anton Nordin <nordinen93@hotmail.com>
2026-03-25 11:57:41 +0100

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ZipTrip.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
