775e8c000306a1bfa51336c612492eacdeb20e25
16 Anton Nordin <nordinen93@hotmail.com>
2026-03-25 16:27:38 +0100

    setup database and map strukture

---
+++ Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ZipTrip.Extensions;
+
+public static class WebApplicationExtensions
+{
+    public static IServiceCollection AddWebServices(this IServiceCollection services)
+    {
+        services.AddRazorPages();
+
+        services.ConfigureHttpJsonOptions(options =>
+        {
+            options.SerializerOptions.PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase;
+            options.SerializerOptions.WriteIndented = true;
+            options.SerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
+        });
+
+        return services;
+    }
+
+    public static WebApplication UseZipTripMiddleware(this WebApplication app)
+    {
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();           
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        return app;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,41 +1,21 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using ZipTrip.Data;
+using ZipTrip.Services.Extensions;
+using ZipTrip.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+// Database + Identity via Services (ren arkitektur)
+builder.Services.AddDatabase(builder.Configuration);
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddRazorPages();
+// Application services
+builder.Services.AddApplicationServices();
+
+// Web-specifika tjänster
+builder.Services.AddWebServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
+// Middleware
+app.UseZipTripMiddleware();
+app.MapRazorPages();
 
 app.Run();
